44ea904ac62ab9880cbc59e5bfc1c7b9c55df1da
Michael Prange <prange@alum.mit.edu>
2026-07-30 17:17:53 -0400

    Initial Commit

---
+++ SaddleBack/SaddleBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaddleBackApp.swift
+//  SaddleBack
+//
+//  Created by Michael Prange on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SaddleBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SaddleBackTests/SaddleBackTests.swift
@@ -0,0 +1,19 @@
+//
+//  SaddleBackTests.swift
+//  SaddleBackTests
+//
+//  Created by Michael Prange on 7/30/26.
+//
+
+import Testing
+@testable import SaddleBack
+
+struct SaddleBackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
